20ee8df232f3f2af8da20065a47a09dc8642b9f6
practice20200 <wor9hard2018@gmail.com>
2022-07-13 06:19:57 -0700

    Refactor the name of default ViewController

--- ContactApp_TestPurpose_July13th/Controllers/WelcomViewController.swift
+++ ContactApp_TestPurpose_July13th/Controllers/WelcomViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomViewController: UIViewController {
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
     }
 
