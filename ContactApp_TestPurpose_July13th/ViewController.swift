3f8d8df5b41cf55d02deed4946e7c5b7692cd352
practice20200 <wor9hard2018@gmail.com>
2022-07-13 05:51:43 -0700

    first commit'

---
+++ ContactApp_TestPurpose_July13th/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp_TestPurpose_July13th
+//
+//  Created by Apple New on 2022-07-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
