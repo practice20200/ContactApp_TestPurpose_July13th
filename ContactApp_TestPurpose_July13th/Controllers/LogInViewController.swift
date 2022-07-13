d93e1569311e52c60fb2b1bed80f67008c118197
practice20200 <wor9hard2018@gmail.com>
2022-07-13 09:14:33 -0700

    Modified the layout in LoginViewController

--- ContactApp_TestPurpose_July13th/Controllers/LogInViewController.swift
+++ ContactApp_TestPurpose_July13th/Controllers/LogInViewController.swift
@@ -6,24 +6,147 @@
 //
 
 import UIKit
+import Elements
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController{
+    
+    lazy var titleLabel: BaseUILabel = {
+        let label = BaseUILabel()
+        label.textColor = .systemBackground
+        label.text = "Contact"
+        label.font = UIFont.systemFont(ofSize: 27)
+        return label
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    lazy var emailTF: BaseUITextField = {
+        let tf = BaseUITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "email",
+                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemBackground])
+        tf.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        tf.layer.shadowColor = UIColor.lightGray.cgColor
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .continue
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    lazy var passTF: BaseUITextField = {
+        let tf = BaseUITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "password",
+                                                      attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemBackground])
+        tf.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        tf.layer.shadowColor = UIColor.lightGray.cgColor
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .done
+        return tf
+    }()
+    
+    lazy var loginButton: BaseUIButton = {
+        let button = BaseUIButton()
+        button.setTitle("Log in", for: .normal)
+        button.setTitleColor(.systemBackground, for: .normal)
+        button.backgroundColor = UIColor.systemMint
+        button.layer.cornerRadius = 20
+        button.layer.shadowOpacity = 0.3
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        button.addTarget(self, action: #selector(loginHandler), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var textFieldStack: VStack = {
+        let stack = VStack()
+        stack.addArrangedSubview(emailTF)
+        stack.addArrangedSubview(passTF)
+        stack.spacing = 20
+        stack.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        return stack
+    }()
 
-        // Do any additional setup after loading the view.
+    lazy var contentStack: VStack = {
+        let stack = VStack()
+        stack.addArrangedSubview(titleLabel)
+        stack.addArrangedSubview(textFieldStack)
+        stack.addArrangedSubview(loginButton)
+        stack.spacing = 30
+        stack.alignment = .center
+        stack.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        return stack
+    }()
+    
+    lazy var scrollView =  VScrollableView(content: contentStack)
+    
+    
+    
+    lazy var gradientLayer : CAGradientLayer = {
+        let color = CAGradientLayer()
+        color.colors = [
+            UIColor(red: 50/255.0, green: 150.0/255.0, blue: 150/255.0, alpha: 1.0).cgColor,
+               UIColor(red: 50.0/255.0, green: 200.0/255.0, blue: 100.0/255.0, alpha: 1.0).cgColor
+        ]
+        return color
+    }()
+    
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        emailTF.setUnderLine()
+        passTF.setUnderLine()
     }
     
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log in"
+        view.backgroundColor = .secondarySystemBackground
+        view.layer.addSublayer(gradientLayer)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        scrollView.showsVerticalScrollIndicator = false
+        
+        emailTF.delegate = self
+        passTF.delegate = self
     }
-    */
+        
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        gradientLayer.frame = view.bounds
+    }
 
+    
+    
+    
+    
+    @objc func loginHandler(){
+        let vc = HomeViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+    
+    
+    
+    
+extension LogInViewController : UITextFieldDelegate{
+    // when users enter, automatically move to the next page
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTF{
+            passTF.becomeFirstResponder()
+        }
+        else if textField == passTF{
+            passTF.resignFirstResponder()
+            print("Done")
+        }
+        return true
+    }
 }
